6ac094079c9a49f580f86961094fa0f954aec29c
pegasis470 <www.sumantdhere@gmail.com>
2025-01-24 15:25:35 +0530

    db config

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "default") 
+SQLALCHEMY_DATABASE_URL = os.getenv("PG_DATABASE_URL", "default") 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
